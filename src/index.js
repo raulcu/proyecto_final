f7753c715b1d65ead83ddfbfac590cdde01f3591
Raul Comun <raulcomununocc@gmail.com>
2025-08-12 01:59:08 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HashRouter } from 'react-router-dom'; // Importa HashRouter
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
